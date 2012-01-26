8abf49e0abd4ea9d0d3cf30d66a000085109fa58
kbl <m.pietraszek@gmail.com>
2012-01-26 14:49:09 +0100

    simpler logger, #1

--- lib/codebar/logger.rb
+++ lib/codebar/logger.rb
@@ -1,17 +1,7 @@
 require 'codebar'
 
 module Codebar
-  module Logger
 
-    extend self
+  Logger = ::Logger.new(STDOUT)
 
-    def debug(message)
-      logger.debug(message)
-    end
-
-    def logger
-      @logger ||= ::Logger.new(STDOUT)
-    end
-
-  end
 end
