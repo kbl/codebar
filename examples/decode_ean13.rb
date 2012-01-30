d613f12ed3ccb7b3b5d9efa873ee819409cee2a6
kbl <m.pietraszek@gmail.com>
2012-01-30 21:12:58 +0100

    feedback: examples are ruby files

--- examples/decode_ean13.rb
+++ examples/decode_ean13.rb
@@ -1,8 +1,6 @@
-#!/usr/bin/env ruby
-
 require_relative '../lib/codebar'
 
 file_path = File.join(File.dirname(__FILE__), '..', 'spec', 'images', 'ean13_clear2.png')
 
 barcode = Codebar::Barcode.new(file_path)
-p barcode.decode
+p barcode.decode(:ean13)
