23504d7de64b1223847c0b20f9e8e8b4a473dcc5
kbl <m.pietraszek@gmail.com>
2012-01-30 20:39:00 +0100

    feedback, cleaning logging, #7

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,5 @@
 require 'codebar'
 
-module Codebar
-
-  Logger = ::Logger.new(nil)
-
-end
-
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
